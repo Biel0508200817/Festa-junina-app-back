23614cc209dba3d5eab0b5f944619157f9bd7ee1
Gabriel Ribeiro de Menezes <gabriel.r.menezes@aluno.senai.br>
2026-05-12 09:17:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -238,9 +238,6 @@ app.use((err, req, res, next) => {
 // ================= LOCAL =================
 
 const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
 
 module.exports = app;
