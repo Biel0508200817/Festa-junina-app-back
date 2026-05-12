106df068c67320bdae20fa2cc7e0fe1353769d66
Gabriel Ribeiro de Menezes <gabriel.r.menezes@aluno.senai.br>
2026-05-12 09:22:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -25,12 +25,7 @@ app.use((req, res, next) => {
 
 // ================= TESTE =================
 
-app.get('/', (req, res) => {
 
-    res.json({
-        status: 'API online'
-    });
-});
 
 // ================= ROTAS =================
 
